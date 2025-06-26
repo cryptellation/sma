94c99585d3a4f0be142e0d032e0ab183750d671f
Louis Fradin <lerenn@users.noreply.github.com>
2025-06-26 14:15:41 +0200

    chores: add DB credentials cleanup functionality (#9)

--- deployments/docker-compose/postgresql/cryptellation_candlesticks.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER cryptellation_candlesticks;
-ALTER USER cryptellation_candlesticks PASSWORD 'cryptellation_candlesticks';
-ALTER USER cryptellation_candlesticks CREATEDB;
-
-CREATE DATABASE cryptellation_candlesticks;
-GRANT ALL PRIVILEGES ON DATABASE cryptellation_candlesticks TO cryptellation_candlesticks;
-\c cryptellation_candlesticks postgres
-GRANT ALL ON SCHEMA public TO cryptellation_candlesticks;
